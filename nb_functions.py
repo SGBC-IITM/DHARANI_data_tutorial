18d66160219f0563f4d79094f2a06fdc4ca47817
sskram <ssk.ram@gmail.com>
2025-05-19 14:33:51 +0530

    adding inline zoomable viewer in dharani_sample.nb

--- nb_functions.py
+++ nb_functions.py
@@ -1,9 +1,10 @@
 
-from IPython.display import display, HTML
+from IPython.display import display, HTML, IFrame
 from matplotlib import pyplot as plt
 from shapely.plotting import plot_polygon
-import json
+# import json
 import shapely
+import numpy as np
 
 from typing import Dict, List
 from ontology_handling import TreeHelper
@@ -124,103 +125,9 @@ def display_annotation_tree(annot:'Annotation', ontohelper:'TreeHelper', selecte
         outstr+'</div>'))
 
 
-#%% for showing jstree
-
-html_head_jstree ="""
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jstree/3.3.12/themes/default/style.min.css" />
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jstree/3.3.12/jstree.min.js"></script>
-    """
-
-html_code_jstree = """
-    <input type="text" id="search_input" placeholder="Search tree..." style="margin-bottom:10px;width:200px;padding:5px;"/>
-    <div id="jstree_demo"></div>
-
-    <script>
-        $(document).ready(function() {
-            // Initialize jstree with search plugin
-            $('#jstree_demo').jstree({
-                
-                'core': {
-                    'multiple' : false,
-                    'animation' : 0,
-                    'themes': {'icons':false,},
-                    'data': %s,
-                },                
-                'plugins': ['search'],
-                'search': {
-                    'show_only_matches':true,
-                    'show_only_matches_children':true,
-                }
-            });
-            
-            // Bind search input to jstree search function
-            $('#search_input').on('keyup', function() {
-                var searchValue = $(this).val();
-                $('#jstree_demo').jstree(true).search(searchValue);
-            });
-        });
-    </script>
-"""
-
-
-def show_jstree(ontohelper:'TreeHelper'):
-    tree_data = json.dumps(ontohelper.treenom)
-
-    display(HTML(html_head_jstree))
-
-    display(HTML(html_code_jstree % tree_data))
-
-
-#%% for showing ol 
-html_head_ol="""
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/openlayers/openlayers.github.io@master/en/v6.14.1/css/ol.css" type="text/css">
-    <script src="https://cdn.jsdelivr.net/npm/ol@v10.4.0/dist/ol.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/geotiff"></script>
-    """
-
-
-html_code_ol = """
-    <div id="map-container"></div>
-    <script type="text/javascript">
-
-    const sourceExtent = [0, 0, 64000, 64000];
-    const olMap = new ol.Map({
-        target: 'map-container',
-        view: new ol.View({
-        zoom: 1,
-          minZoom: 10,
-            maxZoom: 17,
-        })
-      });
-      const geoTiffLayer = new ol.layer.WebGLTile({
-        id: `geoTiffs`,
-        zIndex: 3,
-        source: new ol.source.GeoTIFF({
-          sources: [
-            {
-              url: %s,
-            },
-          ],
-        }),
-      });
-
-      olMap.addLayer(geoTiffLayer);
-      olMap.getView().fit(sourceExtent);
-    </script>
-"""
-
-def show_inline_viewer(imageurl:str):
-    # 'https://dharani-fetal-brain-atlas.s3.us-west-2.amazonaws.com/data2d/specimen_2/Specimen_2_1000.tif'
-    # use helper.get_section_urls
-
-    display(HTML(html_head_ol))
-
-    display(HTML(html_code_ol % imageurl))
-
 
 # a utility function, to grid the page to match tiling
-import numpy as np
+
 
 def display_tiling_grid(thumbnailimg, pageinfo=None):
     plt.imshow(thumbnailimg, extent=[0,pageinfo['imagewidth'], pageinfo['imageheight'], 0])
