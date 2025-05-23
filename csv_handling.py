19a1e58b03636183926f57260c9cb27e930fe0ac
sskram <ssk.ram@gmail.com>
2025-05-23 18:21:51 +0530

    adding other/plot_trend.nb

--- csv_handling.py
+++ csv_handling.py
@@ -15,14 +15,37 @@ def _make_cleaned_headings(headings):
     return cleaned_headings
 
 
-def load_csv(listingfilename):
-    listing={}
+def load_csv(listingfilename, keycol='section_number'):
+    """
+    Loads a CSV file into a dictionary of namedtuples.
+
+    Args:
+        listingfilename: Path to the CSV file.
+        keycol: The name of the column to use as the dictionary key.
+                If None, returns a list of dictionaries. Defaults to 'section_number'.
+
+    Returns:
+        A dictionary where keys are values from `keycol` and values are namedtuples
+        representing rows, or a list of dictionaries if `keycol` is None.
+        
+    """
+    if keycol is None:
+        listing = []
+    else:
+        listing={}
     with open(listingfilename) as listingfile:
         reader = csv.reader(listingfile)
         headings = next(reader)
         RowType = namedtuple('RowType', _make_cleaned_headings(headings))
         for row in reader:
             rec = RowType(*row)
-            listing[int(rec.section_number)]=rec
-
+            if keycol is not None:
+                keyval = getattr(rec,keycol)
+                if keyval.isdigit():
+                    listing[int(keyval)]=rec
+                else:
+                    listing[keyval]=rec
+            else:
+                listing.append(rec._asdict()) # suitable for constructing pandas DataFrame
     return listing
+    
